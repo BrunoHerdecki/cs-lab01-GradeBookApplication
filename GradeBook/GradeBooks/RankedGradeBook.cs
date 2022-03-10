5a0dc2833d1163ada2e8f27010db14b980768316
Bruno Herdecki <Bruno.Herdecki@microsoft.wsei.edu.pl>
2022-03-10 21:49:51 +0100

    Update RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.GradeBooks;
+using GradeBook.Enums;
+using GradeBook;
+
 
 namespace GradeBook.GradeBooks
 {
@@ -9,7 +12,43 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-
+            Type = GradeBookType.Ranked;
+            
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException(" ");
+            }
+            int CountedStudents = Students.Count;
+            double BetterThan = 0;
+            
+           foreach (var student in Students)
+            {
+                if(student.AverageGrade > averageGrade)
+                {
+                    BetterThan++;
+                }                
+                
+            }
+
+
+            if (BetterThan >= 0.8*CountedStudents)
+                return 'A';
+            else if (BetterThan >= 0.6* CountedStudents)
+                return 'B';
+            else if (BetterThan >= 0.4 * CountedStudents)
+                return 'C';
+            else if (BetterThan >= 0.2 * CountedStudents)
+                return 'D';
+            else
+                return 'F';            
+        }
+
+        
     }
+   
 }
+
