8324fb9c6e7acd642d95c2b6e33ef4658dd00e5f
Bruno Herdecki <Bruno.Herdecki@microsoft.wsei.edu.pl>
2022-03-10 22:03:38 +0100

    321321

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -46,8 +46,19 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';            
         }
+        public override void CalculateStatistics()
+        {
 
-        
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
     }
    
 }
